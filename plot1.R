ba16f2dcd19c678aad6db188d3e1a2e3cbd374a2
jessicakohkl <jessicakohkl@gmail.com>
2017-03-19 03:03:29 +0800

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@
 file <- "./household_power_consumption.txt"
 
 ## Reads a file in table format and creates a data frame 
-alldata <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+alldata <- read.table(file, header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
 
 ## Subset the data
 subsetalldata <- alldata[alldata$Date %in% c("1/2/2007","2/2/2007") ,]
@@ -11,10 +11,10 @@ subsetalldata <- alldata[alldata$Date %in% c("1/2/2007","2/2/2007") ,]
 gap <- as.numeric(subsetalldata$Global_active_power)
 
 ## Plot histogram with x and y axis labels and fill it red
-hist(gap, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+hist(gap, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
 
 ## Copy plot to PNG file
-dev.copy(png, file="plot1.png", height=480, width=480)
+dev.copy(png, file = "plot1.png", height = 480, width = 480)
 
 ## Close off PNG device
 dev.off()
--- plot3.R
+++ plot3.R
@@ -2,13 +2,13 @@
 file <- "./household_power_consumption.txt"
 
 ## Reads a file in table format and creates a data frame 
-alldata <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+alldata <- read.table(file, header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
 
 ## Subset the data
 subsetalldata <- alldata[alldata$Date %in% c("1/2/2007","2/2/2007") ,]
 
 ## Converts character vectors
-changedatetime <- strptime(paste(subsetalldata$Date, subsetalldata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+changedatetime <- strptime(paste(subsetalldata$Date, subsetalldata$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
 
 ## Return data as numeric
 submetering1 <- as.numeric(subsetalldata$Sub_metering_1)
@@ -16,15 +16,15 @@ submetering2 <- as.numeric(subsetalldata$Sub_metering_2)
 submetering3 <- as.numeric(subsetalldata$Sub_metering_3)
 
 ## Plot all 3 line charts with y axis label
-plot(changedatetime,submetering1, type="l",ylab="Energy sub metering", xlab="")
-lines(changedatetime,submetering2, type = "l", col = "red")
-lines(changedatetime,submetering3, type = "l", col = "blue")
+plot(changedatetime, submetering1, type="l", ylab = "Energy sub metering", xlab = "")
+lines(changedatetime, submetering2, type = "l", col = "red")
+lines(changedatetime, submetering3, type = "l", col = "blue")
 
 ## Inserting the Legend at the top right
-legend("topright", c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1)
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1)
 
 ## Copy plot to PNG file
-dev.copy(png, file="plot3.png", height=480, width=480)
+dev.copy(png, file = "plot3.png", height = 480, width = 480)
 
 ## Close off PNG device
 dev.off()
